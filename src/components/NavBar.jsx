bd70240075f3857c07bcfa10e01b79e5748c8b0c
Monica Serrano <monicasesal@gmail.com>
2026-06-16 13:17:28 +0200

    corregir error

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import logoImg from '../assets/logo.png'
-import './Navbar.css'
+import './NavBar.css'
 
 export default function NavBar() {
     const navigate = useNavigate()
